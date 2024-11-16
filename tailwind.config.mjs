dd25756d4c9c3ac127828c6920fea4ba138d2a4c
JosephFC1995 <j.flores@comunal.co>
2024-11-15 23:46:34 -0500

    Initial project

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				beige: "#EBE8DC",
+				"beige-two": "#D5BFA1",
+				gris: "#A3A096",
+				brandy: "#B37058",
+				cemento: "#737362",
+				grafito: "#383833",
+			},
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: "2rem",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
