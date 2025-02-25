07379252bbc62012580dddaccf2a7411ad7fe365
Joseph Flores <josefc9512@gmail.com>
2025-02-24 20:45:44 -0500

    update various

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  integrations: [tailwind()],
+	output: "static",
+	integrations: [tailwind(), sitemap()],
 });
